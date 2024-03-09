2e758ba08bd337a093f9c7c0c92ed41449becb6c
Connors Jackson <155480807+connorsjackson@users.noreply.github.com>
2024-03-08 18:21:38 -0800

    Add files via upload

---
+++ WES237a_Final_Project_Code_Rev7 (single pynq with server keypad)/Backups/sc.py
@@ -0,0 +1,32 @@
+import time
+import signal
+import socket
+
+def run_server_C():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('192.168.2.1', 12345))
+    sock.listen()
+    conn, addr = sock.accept()
+    with conn:
+        print("before C")
+        sock_remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        time.sleep(0.0001)
+        sock_remote.connect(('137.110.38.247', 12345)) # TODO: replace with Ramin's IPv4 addr thru VPN
+        while True:
+            time.sleep(0.0001)
+            data = conn.recv(1024)
+            data = data.decode()
+            if (data == 'disconnect'):
+                print("Disconnecting Connors' Server from Ramin's Remote Server")
+                sock_remote.sendall(bytes('disconnect', 'utf-8'))
+                break
+            sock_remote.sendall(bytes(data, 'utf-8'))
+            data = list(data)
+            print("Sent the following list to Ramin's Remote Server: ",data)
+        sock_remote.close()
+    print("Connors' Server Disconnected from Ramin's Remote Server")
+
+if __name__ == '__main__':
+    original_sigint = signal.getsignal(signal.SIGINT)
+    signal.signal(signal.SIGINT, exit)
+    run_server_C()
---
+++ WES237a_Final_Project_Code_Rev7 (single pynq with server keypad)/sc_mod.py
@@ -0,0 +1,31 @@
+import time
+import signal
+import socket
+
+def run_server_C():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('192.168.2.1', 12345))
+    print('Waiting for user to connect.')
+    sock.listen()
+    conn, addr = sock.accept()
+    with conn:
+        print("User is now connected!")
+        while True:
+            time.sleep(0.0001)
+            data = conn.recv(1024)
+            data = data.decode()
+            if (data == 'disconnect'):
+                print("Disconnecting Connors' Server from Ramin's Remote Server")
+                #sock_remote.sendall(bytes('disconnect', 'utf-8'))
+                break
+            elif (len(data) ==4):
+                #sock_remote.sendall(bytes(data, 'utf-8'))
+                data = list(data)
+                print("Sent the following list to Ramin's Remote Server: ",data)
+        #sock_remote.close()
+    print("Connors' Pynq has disconnected from Server")
+
+if __name__ == '__main__':
+    original_sigint = signal.getsignal(signal.SIGINT)
+    signal.signal(signal.SIGINT, exit)
+    run_server_C()
